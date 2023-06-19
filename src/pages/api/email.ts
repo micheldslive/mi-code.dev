c921d54a365a31d5790cfe5f62e91f455b5c0365
Michel Domingos <micheldslive@gmail.com>
2023-06-19 08:58:05 -0300

    chore: releasing cors

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Resend } from 'resend';
 import { emailSchema } from '@/src/schemas';
+import NextCors from 'nextjs-cors';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -8,26 +9,28 @@ export default async function SendMail(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
-    try {
-      const { success } = emailSchema.safeParse(req.body);
-      if (!success) return;
+  await NextCors(req, res, {
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    origin: '*',
+    optionsSuccessStatus: 200
+  });
+  try {
+    const { success } = emailSchema.safeParse(req.body);
+    if (!success) return;
 
-      const data = req.body;
+    const data = req.body;
 
-      await resend.emails.send({
-        from: data.name,
-        to: String(process.env.RESEND_EMAIL_USER),
-        subject: `${data.name} - via micode-dev`,
-        html: `<p><strong>Name:</strong> ${data.name}</p>
+    await resend.emails.send({
+      from: data.name,
+      to: String(process.env.RESEND_EMAIL_USER),
+      subject: `${data.name} - via micode-dev`,
+      html: `<p><strong>Name:</strong> ${data.name}</p>
         <p><strong>Email:</strong> ${data.email}</p>
         <p>${data.message}</p>`
-      });
+    });
 
-      return res.status(200).json({ message: 'Email sent' });
-    } catch (error) {
-      res.status(500).json({ error });
-    }
+    return res.status(200).json({ message: 'Email sent' });
+  } catch (error) {
+    res.status(500).json({ error });
   }
-  return res.status(405).json({ message: 'Method not allowed' });
 }
